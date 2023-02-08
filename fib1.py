4e6ae93a0a10f1467bb95e95ef73e34ae0a24c8a
Salar Hafezi <salar.hfz.work@gmail.com>
2023-02-08 19:55:23 +0330

    add base case to recursive fibo

--- fib1.py
+++ fib1.py
@@ -1,6 +1,9 @@
 def fibonacci(n: int) -> int:
+    if (n > 0 and n < 3): # base (non-recursive) case
+        return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 if __name__ == '__main__':
     print(fibonacci(5))
+    print(fibonacci(10))
 
